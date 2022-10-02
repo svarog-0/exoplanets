43111d41b33da02e3a2dca17f280209315c5f5ae
svarog-0 <b.radivojevic@outlook.com>
2022-10-02 02:44:42 +0200

    Added db.json and data.ts. Removed placeholder hello.ts

---
+++ pages/api/data.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import path from 'path'
+import fsPromises from 'fs/promises';
+
+type Data = {
+  name: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  console.info("Date called")
+  const filePath: string = path.join(process.cwd(), 'data/data.json');
+  res.status(200).json({ name: 'John Doe' })
+  const jsonData = await await fsPromises.readFile(filePath);
+
+  res.status(200).json(jsonData.toJSON)
+}
