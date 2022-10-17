6515568669dcd886a93a87170e7b4e1dd04d6cf6
svarog-0 <b.radivojevic@outlook.com>
2022-10-17 22:04:48 +0200

    Fix get by name

--- pages/api/resolvers/index.ts
+++ pages/api/resolvers/index.ts
@@ -5,8 +5,8 @@ import { getPlanet, getPlanets } from "../../../services/data.service";
 export const resolvers = {
   Query: {
     Planets: async (_: any, args: {pagination: Pagination, filter: PlanetFilter, sort: Sort}) : Promise<Page<Planet>> => getPlanets(args.pagination ,args.filter, args.sort),
-    Planet: async (_: any, args: any) => {
-        const planet: Planet = await getPlanet(args)
+    Planet: async (_: any, args: {name: string}) => {
+        const planet: Planet = await getPlanet(args.name)
         if(!planet) return null
         return planet;
     }
